68d5c7313625427ffaf31a2d79e2ded60781218e
Sai Charan Kamatam <Saicharankamatam@gmail.com>
2023-03-08 11:27:50 +0530

    updated the steps to run the app

--- app.js
+++ app.js
@@ -35,108 +35,113 @@ const main = async () => {
                 temperature: 0
             });
             const data = completion.data.choices[0].text
-            // console.log(completion.data.choices[0]);
-            // if (completion.data.choices.length > 0) {
-            //     if (data) {
-            //         const array = data.split("\n\n").slice(1)
-            //         console.log(array)
-            //         let result = {}
-            //         let multiple = []
-            //         for (let i = 0; i < parseInt(len); i++) {
-            //             let questionArr = array[i].split("\n")
-            //             // console.log(questionArr);
-            //             if (i == 0) {
-            //                 multiple.push({
-            //                     "question": questionArr[2],
-            //                     "optionA": questionArr[3],
-            //                     "optionB": questionArr[4],
-            //                     "optionC": questionArr[5],
-            //                     "optionD": questionArr[6],
-            //                     "answer": questionArr[7], /*.split("Answer: ")[1]*/
-            //                     "Difficulty": questionArr[8], /*.split("Difficulty: ")[1]*/
-            //                     "Explanation": questionArr[9] /*.split("Explanation: ")[1]*/
-            //                 })
-            //             } else {
-            //                 multiple.push({
-            //                     "question": questionArr[0],
-            //                     "optionA": questionArr[1],
-            //                     "optionB": questionArr[2],
-            //                     "optionC": questionArr[3],
-            //                     "optionD": questionArr[4],
-            //                     "answer": questionArr[5], /*.split("Answer: ")[1]*/
-            //                     "Difficulty": questionArr[6], /*.split("Difficulty: ")[1]*/
-            //                     "Explanation": questionArr[7] /*.split("Explanation: ")[1]*/
-            //                 })
-            //             }
-            //             // console.log(multiple);
+            console.log(completion.data.choices[0]);
+            // the output format will be same but the input should be given in a proper format so that you get the optimal results from the chatgpt
+            
+            if (completion.data.choices.length > 0) {
+                if (data) {
+                    const array = data.split("\n\n").slice(1)
+                    // console.log(array)
+                    let result = {}
+                    let multiple = []
+                    for (let i = 0; i < parseInt(len); i++) {
+                        let questionArr = array[i].split("\n")
+                        // console.log(questionArr);
+                        if (i == 0) {
+                            multiple.push({
+                                "question": questionArr[2],
+                                "optionA": questionArr[3],
+                                "optionB": questionArr[4],
+                                "optionC": questionArr[5],
+                                "optionD": questionArr[6],
+                                "answer": questionArr[7], /*.split("Answer: ")[1]*/
+                                "Difficulty": questionArr[8], /*.split("Difficulty: ")[1]*/
+                                "Explanation": questionArr[9] /*.split("Explanation: ")[1]*/
+                            })
+                        } else {
+                            multiple.push({
+                                "question": questionArr[0],
+                                "optionA": questionArr[1],
+                                "optionB": questionArr[2],
+                                "optionC": questionArr[3],
+                                "optionD": questionArr[4],
+                                "answer": questionArr[5], /*.split("Answer: ")[1]*/
+                                "Difficulty": questionArr[6], /*.split("Difficulty: ")[1]*/
+                                "Explanation": questionArr[7] /*.split("Explanation: ")[1]*/
+                            })
+                        }
+                        // console.log(multiple);
 
-            //         }
+                    }
 
-            //         let moreMultiple = []
-            //         for (let i = parseInt(len); i < 2*parseInt(len); i++) {
-            //             let questionArr = array[i].split("\n")
+                    let moreMultiple = []
+                    for (let i = parseInt(len); i < 2 * parseInt(len); i++) {
+                        let questionArr = array[i].split("\n")
 
-            //             if (i == 3) {
-            //                 moreMultiple.push({
-            //                     "question": questionArr[1],
-            //                     "optionA": questionArr[2],
-            //                     "optionB": questionArr[3],
-            //                     "optionC": questionArr[4],
-            //                     "optionD": questionArr[5],
-            //                     "answer": questionArr[6], /*.split("Answer: ")[1]*/
-            //                     "Difficulty": questionArr[7], /*.split("Difficulty: ")[1]*/
-            //                     "Explanation": questionArr[8] /*.split("Explanation: ")[1]*/
-            //                 })
-            //             } else {
-            //                 moreMultiple.push({
-            //                     "question": questionArr[0],
-            //                     "optionA": questionArr[1],
-            //                     "optionB": questionArr[2],
-            //                     "optionC": questionArr[3],
-            //                     "optionD": questionArr[4],
-            //                     "answer": questionArr[5], /*.split("Answer: ")[1]*/
-            //                     "Difficulty": questionArr[6], /*.split("Difficulty: ")[1]*/
-            //                     "Explanation": questionArr[7] /*.split("Explanation: ")[1]*/
-            //                 })
-            //             }
-            //         }
-            //         // console.log(moreMultiple);
-            //         let trueFalse = []
-            //         for (let i = 2*parseInt(len); i < 3*parseInt(len); i++) {
-            //             let questionArr = array[i].split("\n")
-            //             if(i==5){
-            //                 trueFalse.push({
-            //                     "question" : questionArr[1],
-            //                     "answer" : questionArr[2],
-            //                     "Difficulty": questionArr[3], /*.split("Difficulty: ")[1]*/
-            //                     "Explanation": questionArr[4]
-            //                 })
+                        if (i == 3) {
+                            moreMultiple.push({
+                                "question": questionArr[1],
+                                "optionA": questionArr[2],
+                                "optionB": questionArr[3],
+                                "optionC": questionArr[4],
+                                "optionD": questionArr[5],
+                                "answer": questionArr[6], /*.split("Answer: ")[1]*/
+                                "Difficulty": questionArr[7], /*.split("Difficulty: ")[1]*/
+                                "Explanation": questionArr[8] /*.split("Explanation: ")[1]*/
+                            })
+                        } else {
+                            moreMultiple.push({
+                                "question": questionArr[0],
+                                "optionA": questionArr[1],
+                                "optionB": questionArr[2],
+                                "optionC": questionArr[3],
+                                "optionD": questionArr[4],
+                                "answer": questionArr[5], /*.split("Answer: ")[1]*/
+                                "Difficulty": questionArr[6], /*.split("Difficulty: ")[1]*/
+                                "Explanation": questionArr[7] /*.split("Explanation: ")[1]*/
+                            })
+                        }
+                    }
+                    // console.log(moreMultiple);
+                    let trueFalse = []
+                    for (let i = 2 * parseInt(len); i < 3 * parseInt(len); i++) {
+                        let questionArr = array[i].split("\n")
+                        if (i == 5) {
+                            trueFalse.push({
+                                "question": questionArr[1],
+                                "answer": questionArr[2],
+                                "Difficulty": questionArr[3], /*.split("Difficulty: ")[1]*/
+                                "Explanation": questionArr[4]
+                            })
 
-            //             }else{
-            //                 trueFalse.push({
-            //                     "question" : questionArr[0],
-            //                     "answer" : questionArr[1],
-            //                     "Difficulty": questionArr[2], /*.split("Difficulty: ")[1]*/
-            //                     "Explanation": questionArr[3]
-            //                 })
-            // //             }
-                        
-            //         }
-            //         // console.log(trueFalse);
-            //         // console.log(moreMultiple);
-            //         console.log({
-            //            multiple : multiple ,
-            //            trueFalse : trueFalse,
-            //            moreMultiple : moreMultiple
-            //         });
-                    // res.json({ "data": {
-                    //     multiple : multiple ,
-                    //     trueFalse : trueFalse,
-                    //     moreMultiple : moreMultiple
-                    //  } })
+                        } else {
+                            trueFalse.push({
+                                "question": questionArr[0],
+                                "answer": questionArr[1],
+                                "Difficulty": questionArr[2], /*.split("Difficulty: ")[1]*/
+                                "Explanation": questionArr[3]
+                            })
+                            //             }
 
-            //     }
-            // }
+                        }
+                        // console.log(trueFalse);
+                        // console.log(moreMultiple);
+                        // console.log({
+                        //     multiple: multiple,
+                        //     trueFalse: trueFalse,
+                        //     moreMultiple: moreMultiple
+                        // });
+                        // res.json({
+                        //     "data": {
+                        //         multiple: multiple,
+                        //         trueFalse: trueFalse,
+                        //         moreMultiple: moreMultiple
+                        //     }
+                        // })
+
+                    }
+                }
+            }
         } catch (error) {
             console.log(error.message);
             res.json({ "error": error.message })
---
+++ readme.txt
@@ -0,0 +1,10 @@
+after cloning the repo 
+go to the current directory by running cd <directory name>
+
+then use <npm install> in the terminal to install all the required packages into our node app
+update the api key of yours from openai.com in .env file
+then run <node app.js> or <npm start> to run our server 
+
+you will get a message in the console that your serve is <listening>
+
+now you can use your server 
